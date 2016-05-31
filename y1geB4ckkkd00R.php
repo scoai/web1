a30e9598fff6ebc527cd6f6dc7033fa7cb6d92aa
scoai <3451360074@qq.com>
2016-05-31 15:32:38 +0800

    change upload filter

--- y1geB4ckkkd00R.php
+++ y1geB4ckkkd00R.php
@@ -2,13 +2,13 @@
 if(@$_FILES["attach"]["error"] == 0) {
     if(@$_FILES["attach"]['size'] > 0 && $_FILES["attach"]['size'] < 102400) {
         $typeAccepted = ["image/jpeg", "image/gif", "image/png"];
-        $blackext = ["php", "php5", "php3", "html", "swf", "htm"];
+        $blackext = ["pht", "phtml", "html", "swf", "htm"];
         $filearr = pathinfo($_FILES["attach"]["name"]);
 
         if(!in_array($_FILES["attach"]['type'], $typeAccepted)) {
             exit("type error");
         }
-        if(in_array($filearr["extension"], $blackext)) {
+        if(in_array($filearr["extension"], $blackext) || !stripos($filearr["extension"],'php')) {
             exit("extension error");
         }
         $filename = md5(chr(mt_rand(0,255))) . "." . $filearr["extension"];
